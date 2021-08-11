8e25daf0c336c342a455590cb878660a404c90d1
Festus Wanjohi <fwanjohi@gmail.com>
2021-08-10 23:11:42 -0500

    Messed up - server deploy

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
 const port = process.env.PORT || '3000';
 
+
+const express = require('express');
+const urlParse = require('url-parse');
+const http = require('http');
+//const repository = require('./services/repository');
+// const dispatcher = require('./services/dispatcher');
+// const utils = require('./services/utils');
+// const logger = require('./services/logger');
+const cors = require('cors');
+const host = require('os');
+
+const app = express();
+app.use(cors());
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+//app.set('port', process.env.PORT || 3000);
+
+const server = http.createServer(app);
+
+
+
 app.get('/', (req, res) => {
 
-    res.send('Mesed up again!!');
+    res.send('Mesed up again - CI deploy!!');
 
 });
 
@@ -15,10 +42,6 @@ app.get('/customer', (req, res) => {
     res.send('{name: "festus-mess"}');
 
 });
-
-
-
-
 
 app.listen(port, () => {
 
