a536e0f774225228f0a3fabf75bbbc27b01f0fd0
Festus Wanjohi <fwanjohi@gmail.com>
2021-08-12 23:11:38 -0500

    Get IP

--- index.js
+++ index.js
@@ -43,6 +43,42 @@ app.get('/', (req, res) => {
 
 });
 
+app.get('/config', (req, res) => {
+    //const ip = require("ip");
+    const { networkInterfaces } = require('os');
+
+    const nets = networkInterfaces();
+    const results = Object.create(null); // Or just '{}', an empty object
+
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            if (net.family === 'IPv4' && !net.internal) {
+
+                if (!results[name]) {
+                    results[name] = [];
+                }
+                results[name].push(net.address);
+
+
+
+                if (name == "vEthernet (WSL)") {
+                    results['ip'] = net.address;
+                }
+
+            }
+        }
+    }
+    results["port"] = port;
+    // const config = {
+    //     port: port,
+    //     ip: ip
+    // };
+    res.send(results);
+
+});
+
+
 app.get('/test', (req, res) => {
 
     res.send('{name: "festus-mess"}');
